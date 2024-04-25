bac5713d61927e1526ec10d55b2fdf1c2a495b7b
therexcodes <therexui@gmail.com>
2024-04-25 18:39:38 +0000

    second

--- UserAuth/settings.py
+++ UserAuth/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kl-ch$=*1!u0zh110#=hcaz)=ry*qe*t-_+g-x-4$jx&@q@doy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,11 +115,15 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
